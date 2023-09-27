32b5df24bcbe4b1302fbaa84fc5497f9c5871fc5
Snibaw <Snibaw@gmail.com>
2023-09-27 17:38:05 +0200

    Double Jump + correct ground position

--- PlatformerDeveloppement1/Assets/Scripts/InputController.cs
+++ PlatformerDeveloppement1/Assets/Scripts/InputController.cs
@@ -9,6 +9,7 @@ public class InputController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
         playerMovement = GetComponent<PlayerMovement>();
     }
 
--- PlatformerDeveloppement1/Assets/Scripts/PlayerMovement.cs
+++ PlatformerDeveloppement1/Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
     private float lastX;
     private BoxCollider2D boxCollider;
     private bool isCollidingWithObstacle = false;
+    private bool hasCollideX = false;
 
     [Header("Dash")]
     //Dash
@@ -28,14 +29,18 @@ public class PlayerMovement : MonoBehaviour
     private float speedX;
     private float speedY;
     [SerializeField] private float detectionCollisionOffsetY = 0.1f;
-    [SerializeField] private bool isGrounded;
+    private bool isGrounded;
     [SerializeField] private float maxJumpPower = 2;
     [SerializeField] private float decayJumpPower = 0.1f;
     private float jumpPower = 0;
     [SerializeField] private float maxJumpTime = 1;
     [SerializeField] private float gravity = 9.81f;
+    private bool canDoubleJump = true;
     private bool isJumping;
     private float timeJumpButtonPressed;
+    private bool hasCollideY = false;
+    // [SerializeField] private float testvalue = 1;
+    
 
 
     private void Start() 
@@ -50,24 +55,46 @@ public class PlayerMovement : MonoBehaviour
     {
         dashCooldownTimer -= Time.deltaTime;
 
-        CheckGrounded();
+        GroundManagement();
+
+        GravityManagement();
+    }
+    private void GravityManagement()
+    {
         if (!isGrounded)
             speedY -= gravity * Time.deltaTime;
         else
             speedY = Mathf.Max(0, speedY);
         transform.Translate(new Vector3(0, speedY, 0));
     }
-    public void CheckGrounded()
+    private void GroundManagement()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0, -1), detectionCollisionOffsetY + boxCollider.size.y / 2);
+        float raycastLength = boxCollider.size.y/4 + Mathf.Abs(speedY) * Time.deltaTime;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(0, boxCollider.bounds.extents.y), Vector2.down, raycastLength);
+        Debug.DrawRay(transform.position - new Vector3(0, boxCollider.bounds.extents.y), new Vector2(0, -1) * raycastLength, Color.red);
+        Debug.Log(raycastLength);
 
         if (hit.collider != null)
         {
-            Debug.Log(hit.collider.name);
-            isGrounded = true;
+            if(hit.collider.gameObject.tag == "Obstacle")
+            {
+                canDoubleJump = true;
+                isGrounded = true;
+                if(hasCollideY == false)
+                {
+                    hasCollideY = true;
+                    transform.position = new Vector3(transform.position.x, hit.point.y + detectionCollisionOffsetY +boxCollider.size.y / 4 , transform.position.z);
+                }
+            }
+            else
+            {
+                hasCollideY = false;
+                isGrounded = false;
+            }
         }
         else
         {
+            hasCollideY = false;
             isGrounded = false;
         }
     }
@@ -81,7 +108,7 @@ public class PlayerMovement : MonoBehaviour
 
             //Check if there is an obstacle in this direction
             Vector3 raycastDirection = new Vector3(lastX, 0, 0);
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, raycastDirection, dashDistance + detectionCollisionOffsetX + boxCollider.size.x/2);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, raycastDirection, dashDistance + boxCollider.size.x/2);
 
             if(hit.collider != null)
             {
@@ -107,22 +134,31 @@ public class PlayerMovement : MonoBehaviour
         //Check if there is an obstacle in this direction     
         Vector3 raycastDirection = new Vector3( x == 0 ? lastX : x, 0, 0);
         //Draw Raycast depending on the direction of the player
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, raycastDirection, detectionCollisionOffsetX + boxCollider.size.x/2);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, raycastDirection, boxCollider.size.x/2 + Mathf.Abs(x) * speed * Time.deltaTime);
 
         if(hit.collider != null)
         {
             if(hit.collider.gameObject.tag == "Obstacle")
             {   
+                Debug.Log(hit.point);
                 isCollidingWithObstacle = true;
+                if(hasCollideX == false)
+                {
+                    hasCollideX = true;
+                    transform.position += new Vector3(Mathf.Sign(x)*(hit.distance - detectionCollisionOffsetX -boxCollider.size.x/2), 0, 0);
+                }
+                
             }
             else
             {
                 isCollidingWithObstacle = false;
+                hasCollideX = false;
             }
         }
         else
         {
             isCollidingWithObstacle = false;
+            hasCollideX = false;
         }
         
         // Move the player when there is no obstacle and an input
@@ -155,8 +191,16 @@ public class PlayerMovement : MonoBehaviour
     {
         if(!isGrounded) 
         {
-            timeJumpButtonPressed = maxJumpTime+1;
-            return;
+            if(!canDoubleJump)
+            {
+                //Avoid hold jump button condition
+                timeJumpButtonPressed = maxJumpTime;
+                return;
+            }
+            else
+            {
+                canDoubleJump = false;
+            }
         }
         timeJumpButtonPressed = Time.deltaTime;
         jumpPower = maxJumpPower;
