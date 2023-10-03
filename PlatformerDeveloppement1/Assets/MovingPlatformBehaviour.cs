25d9a06e630a689da00d8d42b13043e8a3b19bf6
Snibaw <Snibaw@gmail.com>
2023-10-03 15:10:33 +0200

    MovingPlatform

---
+++ PlatformerDeveloppement1/Assets/MovingPlatformBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformBehaviour : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private Transform currentTarget;
+    [SerializeField] private float speed = 0.1f;
+    private int currentIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTarget = waypoints[currentIndex];
+    }
+
+    private void Update() {
+        if (Vector3.Distance(transform.position, currentTarget.position) < 0.1f) {
+            currentIndex++;
+            if (currentIndex >= waypoints.Length) {
+                currentIndex = 0;
+            }
+            currentTarget = waypoints[currentIndex];
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, speed);
+    }
+}
